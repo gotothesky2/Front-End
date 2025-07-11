4bc4d24437939ee6ed92495f2c572d086392efd3
taeyeon0911 <sta0911@kau.kr>
2025-07-11 21:43:38 +0900

    MainPartial completion

--- can-fly-react/src/pages/Home/Home.js
+++ can-fly-react/src/pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import "../../styles/Home.css";
 import Banner from './Banner';
+import Main from './Main';
 
 const Home = () => {
     return (
         <>
             <div className='home-container'>
                 <Banner />
+                <Main/>
                 
             </div>
             
