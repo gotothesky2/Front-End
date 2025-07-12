a26da31eecf3860db645827ab439674ce9742be7
jungyeongmo7 <ballondor7@naver.com>
2025-07-13 01:34:33 +0900

    feat:report

--- can-fly-react/src/App.js
+++ can-fly-react/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Home from './pages/Home/Home';
+import Report from './pages/Report/Report';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from './components/Footer';
 import Header from './components/Header';
@@ -12,6 +13,7 @@ function App() {
       <Routes>
        
         <Route path="/" element={<Home />} />
+        <Route path='/report' element={<Report />} />
       </Routes>
       <Footer/>
     </Router>
---
+++ can-fly-react/src/pages/Report/Report.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Report = () => {
+    return(
+        <div>
+            <h1>레포트</h1>
+            <p>멋쟁이 사자처럼 13기</p>
+        </div>
+    );
+};
+
+export default Report;
