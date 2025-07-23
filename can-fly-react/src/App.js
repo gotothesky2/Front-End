fadeef2a50bf97b54ed7911e9d40f1fd81ad7ccd
kimyena123 <kimyena4930@kau.kr>
2025-07-23 14:32:23 +0900

    충돌 수정

--- can-fly-react/src/App.js
+++ can-fly-react/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Home from './pages/Home/Home';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home/Home';
 import Report from './pages/Report/Report';
@@ -8,6 +9,9 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import TestSelectPage from './pages/TestSelectPage';
 import AptitudeTest from './pages/AptitudeTest';
+import InterestTest from './pages/InterestTest';
+import InterestTestPage7 from './pages/InterestTestPage7';
+import TokenChargePage from './pages/TokenChargePage';
 
 function App() {
   return (
@@ -22,6 +26,9 @@ function App() {
         <Route path='/report' element={<Report />} />
         <Route path="/gradeinput" element={<StudentGrade />} />
         <Route path="/gradeinput/exam" element={<ExamGrade />} />
+        <Route path="/interesttest" element={<InterestTest />} />
+        <Route path="/interesttestpage7" element={<InterestTestPage7 />} />
+        <Route path="/tokencharge" element={<TokenChargePage />} />
       </Routes>
       <Footer/>
     </Router>
