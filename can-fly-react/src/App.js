40ee51dfe1db0f6ba8daa022dbb6cf5e1e64c131
kimyena123 <kimyena4930@kau.kr>
2025-07-23 14:34:05 +0900

    quick fix

--- can-fly-react/src/App.js
+++ can-fly-react/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Home from './pages/Home/Home';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './pages/Home/Home';
 import Report from './pages/Report/Report';
 import StudentGrade from './pages/GradeInput/StudentGrade';
 import ExamGrade from './pages/GradeInput/ExamGrade';
