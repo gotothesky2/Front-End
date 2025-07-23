293eb923a7e36edf264892578fb648d2067b9307
jungyeongmo7 <ballondor7@naver.com>
2025-07-23 14:04:56 +0900

    ...

--- can-fly-react/src/App.js
+++ can-fly-react/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home/Home';
 import Report from './pages/Report/Report';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import StudentGrade from './pages/GradeInput/StudentGrade';
+import ExamGrade from './pages/GradeInput/ExamGrade';
 import Footer from './components/Footer';
 import Header from './components/Header';
 
@@ -14,6 +16,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path='/report' element={<Report />} />
+        <Route path="/gradeinput" element={<StudentGrade />} />
+        <Route path="/gradeinput/exam" element={<ExamGrade />} />
       </Routes>
       <Footer/>
     </Router>
---
+++ can-fly-react/src/components/StudentGradeSidebar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../styles/StudentGradeSidebar.css';
+
+const StudentGradeSidebar = ({ onOpenModal }) => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div className="sidebar-box">
+        <h3>성적 유형</h3>
+        <button className="selected">학생부</button>
+        <button onClick={() => navigate('/gradeinput/exam')}>수능/모의고사</button>
+      </div>
+
+      <div className="sidebar-box">
+        <h3>성적 입력</h3>
+        <p>1학년</p>
+        <button onClick={() => onOpenModal('1학년 1학기')}>1학년 1학기 입력</button>
+        <button onClick={() => onOpenModal('1학년 2학기')}>1학년 2학기 입력</button>
+        <p>2학년</p>
+        <button onClick={() => onOpenModal('2학년 1학기')}>2학년 1학기 입력</button>
+        <button onClick={() => onOpenModal('2학년 2학기')}>2학년 2학기 입력</button>
+        <p>3학년</p>
+        <button onClick={() => onOpenModal('3학년 1학기')}>3학년 1학기 입력</button>
+        <button onClick={() => onOpenModal('3학년 2학기')}>3학년 2학기 입력</button>
+      </div>
+    </>
+  );
+};
+
+export default StudentGradeSidebar;
+
