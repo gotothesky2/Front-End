4f8d9ce2196c8da82b34cf5d897b7e08ffd310b7
jungyeongmo7 <ballondor7@naver.com>
2025-08-11 15:17:36 +0900

    최신판 SWAGGER 까지 반영했음

--- can-fly-react/src/config.js
+++ can-fly-react/src/config.js
@@ -11,6 +11,7 @@ const config = {
 
     USERS: {
         REISSUE_TOKEN: `${BASE_URL}/users/reissue`, // 토큰 재발급
+        USE_RECHARGE: `${BASE_URL}/users/token`, // 토큰 사용 및 충전
         DELETE_ME: `${BASE_URL}/users/me`, // 회원탈퇴
         LOGOUT: `${BASE_URL}/users/logout`, // 로그아웃
     },
@@ -22,7 +23,25 @@ const config = {
         SCORE_REGISTER: mockId => `${BASE_URL}/users/grades/mock/${mockId}`,
         DELETE_MOCK: mockId => `${BASE_URL}/users/grades/mock/${mockId}`,
         SPECIFIC_SCORE_DETAIL: (mockId, mockScoreId) => `${BASE_URL}/users/grades/mock/${mockId}/${mockScoreId}`,
+        EDIT_INFORMATION_MOCK: mockId => `${BASE_URL}/users/grades/mock/${mockId}`,
+        EDIT_SCORE: (mockId, mockScoreId) => `${BASE_URL}/users/grades/mock/${mockId}/${mockScoreId}`,
     },
+
+    REPORT: {
+        ALL_DETAIL_REPORT: `${BASE_URL}/users/grades/report`,
+        REGISTER_REPORT: `${BASE_URL}/users/grades/report`,
+        SPECIFIC_DETAIL_REPORT: (reportId) => `${BASE_URL}/users/grades/report/${reportId}`,
+        SCORE_REGISTER_REPORT: reportId => `${BASE_URL}/users/grades/reprt/${reportId}`,
+        DELETE_REPORT: reportId => `${BASE_URL}/users/grades/report/${reportId}`,
+        SPECIFIC_SCORE_DETAIL_REPORT: (reportId, reportScoreId) => `${BASE_URL}/users/grades/mock/${reportId}/${reportScoreId}`,
+        EDIT_INFORMATION_REPORT: reportId => `${BASE_URL}/users/grades/mock/${reportId}`,
+        EDIT_SCORE_REPORT: (reportId, reportScoreId) => `${BASE_URL}/users/grades/mock/${reportId}/${reportScoreId}`,
+    },
+
+    UNIVERSITY_MAJOR:{
+        SYNCHRONIZE: `${BASE_URL}/major_sync/all`,
+    },
+
 
 };
 
