4ea39cf7145c29d0e7bc239b668cb7a61cea08d2
jungyeongmo7 <ballondor7@naver.com>
2025-08-11 14:57:44 +0900

    Aiconfig.js 만들었음

---
+++ can-fly-react/src/Aiconfig.js
@@ -0,0 +1,29 @@
+const BASE_URL = 'http://43.200.79.118/docs#/';
+
+const AIconfig = {
+    API_URL: BASE_URL,
+
+    CERTIFY: {
+        TEST_TOKEN: `${BASE_URL}/auth/test-token`,
+        ME: `${BASE_URL}/auth/me`, 
+        VERIFY: `${BASE_URL}/auth/verify`,
+        VERIFY_TOKEN: `${BASE_URL}/auth/verify-token`,
+    },
+
+    INTEREST: {
+        MAKE_HMT: `${BASE_URL}/hmt`, 
+        MY_HMT: `${BASE_URL}/hmt/my`, 
+        CHECK_HMT: hmt_id => `${BASE_URL}/hmt/${hmt_id}`,
+        DELETE_HMT: hmt_id => `${BASE_URL}/hmt/${hmt_id}`,
+    },
+
+    APTITUDE: {
+        MAKE_CST: `${BASE_URL}/cst`,
+        MY_CST: `${BASE_URL}/cst/my`,
+        CHECK_CST: cst_id => `${BASE_URL}/cst/${cst_id}`,
+        DELETE_CST: cst_id => `${BASE_URL}/cst/${cst_id}`,
+    },
+
+};
+
+export default AIconfig;
